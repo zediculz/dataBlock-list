6c0e1970c25fd9447d08714a79f6c7b3e1f5bd92
[zedic] <oluwaseyifunmitan1@gmail.com>
2025-04-12 17:04:36 +0100

    list init

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 
 interface ListType {
-  hash: number
+  hash: string
   data: any | undefined 
   timeStamp: string 
   text: string|null
@@ -9,10 +9,55 @@ interface ListType {
 class List {
   lists:ListType[]
   constructor(data: any) {
-    console.log(typeof data, data?.length)
     this.lists = []
+    this.#init(data)
+  }
+
+  /**@method init in-bound constructor */
+  #init(data:any) {
+    const type = typeof data
+    const length = data?.length
+
+    if (type === "object" && length !== undefined) {
+      const nd:ListType[] = []
+      data?.flatMap((d: any, i: number) => {
+
+        const list:ListType = {
+          hash: `${Math.floor(Math.random() * 9999999999)}`,
+          timeStamp: new Date().toDateString(),
+          data: d,
+          text: i === 0 ? "first memeber of the list" : null
+        }
+
+        nd.push(list)
+      })
+
+     this.lists = nd
+    } else {
+      const list:ListType = {
+        hash: `${Math.floor(Math.random() * 9999999999)}`,
+        timeStamp: new Date().toDateString(),
+        data,
+        text: "first memeber on the list"
+      }
+
+      this.lists = [list]
+    }
   }
 }
 
-const list = new List("oo")
+
+const todoList = [{
+  id: 0,
+  todo: "clean my phone",
+  status: false
+},
+{
+  id: 1,
+  todo: "clean my PC",
+  status: false
+}]
+
+
+const list = new List(todoList)
 console.log(list)
